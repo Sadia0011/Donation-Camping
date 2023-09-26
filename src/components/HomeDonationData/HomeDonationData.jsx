0ad7ac797dc804e075121659e7fe43a148a6811f
sadia sultana <sadiamoon2019@gmail.com>
2023-09-26 16:46:14 +0600

    added search button functional in home page

--- src/components/HomeDonationData/HomeDonationData.jsx
+++ src/components/HomeDonationData/HomeDonationData.jsx
@@ -7,12 +7,12 @@ const HomeDonationData = ({donationData}) => {
     return (
         <div>
             <Link to={`/donationId/${id}`}>
-        <div className="relative flex flex-col h-[380px] rounded-xl bg-clip-border shadow-md"
+        <div className="relative flex flex-col h-[340px] rounded-xl bg-clip-border shadow-md"
         style={{color:card_bg_color , backgroundColor:category_bg_color}}>
             <img src={picture} className='h-56' alt="img-blur-shadow" />
             <div className="p-6">
               <h5 className="mb-2 block text-sm font-medium "><span style={{backgroundColor:text_button_bg_color,borderRadius:"8px",padding:"10px"}}>{category}</span></h5>
-              <p className="block  text-lg font-semibold">{title}</p>
+              <h3 className="block mt-2 text-lg font-semibold">{title}</h3>
             </div>
         </div>
             </Link>
